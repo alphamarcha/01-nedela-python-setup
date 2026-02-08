0dabff4e58ff4e3e4ae7c4fa978ee49f2c326f7c
alphamarcha <m.kirillovs@inbox.lv>
2026-02-08 19:05:10 +0200

    Add files via upload

--- hello.py
+++ hello.py
@@ -1,1 +1,14 @@
-print('Hello from VS Code'); 
+print("Hello for VS Code")
+print("My name is Marks")
+print("I am 26 year old")
+print("""I am from Jelgava
+But now i live in Riga""") #dzīvesvieta
+print(1)
+print(2)
+print(3)
+print(17 + 13)
+print("I am",26,"years old.") #Man ir 26 gadi
+x = "Marks"
+y = "Es esmu"
+print(y); 
+print(x)
